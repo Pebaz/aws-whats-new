134dddfe53e5d6cdd9da4e04ec4a4b69fe8cd19c
Pebaz <pebazium@gmail.com>
2020-02-24 13:55:04 -0500

    cli

--- aws-whats-new.py
+++ aws-whats-new.py
@@ -1,32 +1,56 @@
-import tempfile, webbrowser, feedparser, yaml
+import sys, argparse, tempfile, webbrowser, feedparser, yaml
+from pathlib import Path
 
+# Parse CLI arguments
+parser = argparse.ArgumentParser(description='Scrape AWS RSS feed for updates.')
+parser.add_argument('--html', action='store_true', required=False)
+parser.add_argument('--outfile', required=False, type=Path)
+settings = parser.parse_args(sys.argv[1:])
+
+# Define RSS feed source and load profiles config file
 RSS_FEED = 'https://aws.amazon.com/new/feed/'
+PROFILES = yaml.load(Path('profiles.yml').read_text(), Loader=yaml.FullLoader)
 
-with open('profiles.yml') as profiles_file:
-    PROFILES = yaml.load(profiles_file.read(), Loader=yaml.FullLoader)
-    print(PROFILES)
-
+# Fetch new articles from RSS
+print('Fetching new articles from RSS...')
 blog_posts_by_profile = {profile : [] for profile in PROFILES}
+blog_posts_by_profile['uncategorized'] = []
 
-html_file = tempfile.mktemp(suffix='.html')
-with open(html_file, 'w') as file:
-    file.write('<ul>\n')
+for entry in feedparser.parse(RSS_FEED).entries:
+    title = entry['title']
+    for profile in PROFILES:
+        for service in PROFILES[profile]:
+            if service.lower() in title.lower():
+                blog_posts_by_profile[profile].append((
+                    entry['link'], profile, title
+                ))
+                print(f'[{profile}]', title)
 
-    for entry in feedparser.parse(RSS_FEED).entries:
-        title = entry['title']
-        for profile in PROFILES:
-            for service in PROFILES[profile]:
-                if service.lower() in title.lower():
-                    blog_posts_by_profile[profile].append((
-                        entry['link'], profile, title
-                    ))
-                    print(f'[{profile}]', title)
+    else:
+        blog_posts_by_profile['uncategorized'].append((
+            entry['link'], None, title
+        ))
 
-    # Ensure bullet points are sorted by profile
-    for profile in blog_posts_by_profile:
-        for post in blog_posts_by_profile[profile]:
-            file.write('<li><a href="{}">[{}] {}</a></li>\n'.format(*post))
+# Generate html file for easy copy and pasting
+if settings.html:
+    html_file = settings.outfile or tempfile.mktemp(suffix='.html')
 
-    file.write('</ul>\n')
+    with open(html_file, 'w') as file:
+        file.write('<ul>\n')
 
-webbrowser.open(f'file://{html_file}')
+        # Ensure bullet points are sorted by profile
+        for profile in blog_posts_by_profile:
+            for post in blog_posts_by_profile[profile]:
+                file.write('<li><a href="{}">[{}] {}</a></li>\n'.format(*post))
+
+        file.write('</ul>\n')
+
+    webbrowser.open(f'file://{html_file}')
+
+# Display skipped articles
+print(blog_posts_by_profile['uncategorized'])
+for (url, _, title) in blog_posts_by_profile['uncategorized']:
+     print(title)
+
+     aws = title.split('AWS')
+     amazon = title.split('Amazon')
